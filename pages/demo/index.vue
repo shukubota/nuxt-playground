4f4646a70b7dc796816e806433e8fad7e5789be7
shukubota <kubotashu0421@gmail.com>
2022-07-30 23:51:50 +0900

    sample作成

--- pages/demo/index.vue
+++ pages/demo/index.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
-    demo
+    demo count: {{ counter }}
+    <button @click="dummyUpdate">たす</button>
     <Sample
-        :test="a"
-        :foo="foo"
-        :readersNumber="readersNumber"
-        :book="book"
-        :onClick="onClick"
+      :test="a"
+      :readersNumber="readersNumber"
+      :book="book"
+      :onClick="onClick"
     />
   </div>
 </template>
-<script setup>
+<script setup lang="ts">
 import Sample from "./sample"
-import {reactive, ref} from "vue";
+import {computed, reactive, ref, Ref} from "vue";
+import { useCounter } from '../../hooks/useCounter';
+import {useStore} from "vuex";
 
 definePageMeta({
   title: 'My home page'
@@ -23,6 +25,22 @@ const a = ref(1)
 const readersNumber = ref(0);
 const book = reactive({ title: 'Vue 3 Guide' })
 console.log(route.meta.title);
+// console.log(this.$store)
+const store = useStore();
+console.log({ store })
+console.log('render demo')
+
+const { counter, updateCounter } = useCounter();
+
+function setup(props) {
+  console.log('setup in demo')
+  console.log({ props });
+}
+
+function dummyUpdate() {
+  updateCounter()
+}
+
 function onClick() {
   console.log("onclick")
   readersNumber.value = readersNumber.value + 1
--- pages/demo/sample/index.vue
+++ pages/demo/sample/index.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
-    sample: {{ test }}
+    sample text: {{ text }}
     readersNumber: {{ readersNumber }}
     <button @click="onClick">足す</button>
   </div>
 </template>
 <script>
-import {onMounted, onUnmounted, onUpdated, reactive, ref} from 'vue'
+import {onMounted, onUnmounted, onUpdated, reactive, ref, nextTick} from 'vue'
+const text = ref('default');
 export default {
   name: "Sample",
   props: {
@@ -16,14 +17,21 @@ export default {
     book: Object,
     onClick: Function,
   },
+  data() {
+    return {
+      text,
+    }
+  },
   setup(props) {
     console.log({ props });
     console.log(props.book)
+    Promise.resolve()
     onMounted(() => {
       console.log('mounted!')
+      text.value = "mounted"
     })
     onUpdated(() => {
-      console.log('updated!')
+      console.log('onupdated!')
     })
     onUnmounted(() => {
       console.log('unmounted!')
