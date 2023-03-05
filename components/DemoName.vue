0a723fca521d45dba87bf42be85bc7bbb70bbd1d
shukubota <kubotashu0421@gmail.com>
2023-03-06 01:03:12 +0900

    view

---
+++ components/DemoName.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>message: {{ props.message }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+interface Props {
+  message: string;
+}
+const props = defineProps<Props>()
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/DemoNameForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <form @submit.prevent="props.onSubmit">
+      <input type="text" name="name" v-model="inputName"/>
+      <button type="submit" style="margin-left: 10px">
+        送信
+      </button>
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import {Ref} from "vue";
+
+interface Props {
+  onSubmit: (e: any) => void;
+  inputValue: string;
+}
+const props = defineProps<Props>()
+const inputName = ref<string>("")
+</script>
+
+<style scoped>
+
+</style>
