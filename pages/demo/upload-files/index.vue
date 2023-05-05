c54405d4e0df8b12fda84048295aa4c9b835e80e
shukubota <kubotashu0421@gmail.com>
2023-05-05 19:57:40 +0900

    file uploadサンプル

---
+++ pages/demo/upload-files/index.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+const dropFile = (e: DragEvent) => {
+  console.log(e);
+  console.log(e.dataTransfer?.files);
+};
+</script>
+
+<template>
+  <div>
+    <div>
+      <p>uploadサンプル</p>
+    </div>
+    <div @dragover.prevent="" @drop.prevent="dropFile" class="upload-area">
+      <p>アップロード</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.upload-area {
+  width: 100px;
+  height: 100px;
+  background-color: red;
+}
+</style>
